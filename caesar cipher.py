e1be838efcf4efee9acda0070aff19e0ba4f03b3
SamiraFadaei <samirfadaie311@gmail.com>
2026-06-29 10:57:14 +0330

    caesa

---
+++ caesar cipher.py
@@ -0,0 +1,18 @@
+#def greet(): 
+ # print("hello , welcom to python")
+
+#greet()
+
+#def greet_with_name(name):
+  # print(f"hello, {name}")
+ #  print("welcom to python")
+
+
+#greet_with_name("samira")
+def greet_with(name,location):
+  print(f"hello,{name}")
+  print(f"how is the weather in {location}")
+
+#greet_with("samira","iran")
+#greet_with(name="samira", location="iran")
+alphabet=
