6958f2caed9a5b6ead802b39ecb803d200293a53
SamiraFadaei <samirfadaie311@gmail.com>
2026-07-07 13:17:22 +0330

    menu

---
+++ coffee matchine.py
@@ -0,0 +1,24 @@
+MENU  ={
+  "espersso":{
+    "ingrediens":{
+      "water":50,
+      "coffee":18
+    },
+    "cost": 1.5
+  },
+  "latte": {
+      "ingrediens":{
+      "water":200,
+      "milk":150,
+      "coffee":24
+    },
+    "cost": 2.5
+  },
+  "cappucino":{
+     "ingrediens":{
+      "water":250,
+      "milk":100,
+      "coffee":24
+  },
+  "cost": 3.0
+}
